3a9fdc0403f99e2575690600169b4d862654ec05
Leslie Reyes <l.reyes.ubiqum@gmail.com>
2020-09-20 13:06:54 +0200

    implement a form to get input from user

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
 import './App.css';
-import Card from './components/Card'
+import CardList from './containers/CardList';
+import Form from './components/Form/Form'
 class App extends Component {
+ state={
+      profiles: []
+    }
+  
+  addNewProfile = (profileData) => {
+    //console.log('App', profileData)
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData]
+    }))
+  }
   render(){
+    
     return(
       <div>
         <div className="header">
         {this.props.title}
         </div>
-        <Card/>
+        <Form onSubmit={this.addNewProfile}/>
+        <CardList profiles={this.state.profiles}/>
       </div>
       
     )
---
+++ src/components/Form/Form.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import './Form.css'
+class Form extends Component{
+ 
+    state={
+        userName: ''
+    }
+    handleSubmit= async (e) => {
+        e.preventDefault();
+        const response = await axios.get(`https://api.github.com/users/${this.state.userName}`);
+        this.props.onSubmit(response.data);
+        this.setState({
+            userName: ''
+        })
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                value={this.state.userName}
+                onChange={e=>this.setState({userName: e.target.value})}
+                type="text" 
+                placeholder="GitHub username" 
+                required/>
+                <button>Add Card</button>
+            </form>
+        )
+    }
+}
+
+export default Form;
